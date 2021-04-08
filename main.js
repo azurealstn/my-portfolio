a6a1fbfaace4e5da4baef4dfc296c59b3b0db3ad
azurealstn <azurealstn@naver.com>
2021-04-08 15:51:19 +0900

    animation

--- main.js
+++ main.js
@@ -11,13 +11,69 @@ document.addEventListener('scroll', () => {
   }
 });
 
-
+//메뉴 버튼을 클릭했을 때 해당 섹션으로 이동
 const menu = document.querySelector('.navbar__menu');
 menu.addEventListener('click', (event) => {
+  scrollIntoSection(event);
+  const active = document.querySelector('.navbar__menu li.active');
+  active.classList.remove('active');
+  event.target.classList.add('active');
+});
+
+//CONTACT ME 버튼을 클릭했을 때 Contact 섹션으로 이동
+const homeBtn = document.querySelector('.home__button');
+homeBtn.addEventListener('click', (event) => {
+  scrollIntoSection(event);
+});
+
+//버튼 클릭시 스크롤 Up!
+const arrowUp = document.querySelector('.arrow-up');
+arrowUp.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
+document.addEventListener('scroll', () => {
+  arrowUp.style.opacity = "1";
+  arrowUp.style.pointerEvents = "auto";
+  if (window.scrollY === 0) {
+    arrowUp.style.opacity = "0";
+    arrowUp.style.pointerEvents = "none";
+  }
+})
+
+//프로젝트 필터링
+const workCategory = document.querySelector('.work__categories');
+const workProject = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workCategory.addEventListener('click', (event) => {
+  const key = event.target.dataset.key || event.target.parentNode.dataset.key;
+  if (key === null || key === undefined) return;
+  /* console.log(key); */
+
+  //선택된 버튼의 active를 없애고 다음 클릭된 버튼에게 active로 선택
+  const active = document.querySelector('.category__btn.active');
+  active.classList.remove('active');
+  const target = event.target.nodeName === 'BUTTON' ? event.target : event.target.parentNode;
+  target.classList.add('active');
+
+  workProject.classList.add('animate-on');
+
+  setTimeout(() => {
+    projects.forEach(project => {
+      const value = project.dataset.value;
+      if (key === '*' || key === value) project.classList.remove('invisible');
+      else project.classList.add('invisible');
+    });
+    workProject.classList.remove('animate-on');
+  }, 300);
+});
+
+
+function scrollIntoSection(event) {
   const target = event.target;
   const link = target.dataset.link;
-  if (link === null || undefined) return;
+  if (link === null || link === undefined) return;
 
   const scrollTo = document.querySelector(link);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
-});
+}
