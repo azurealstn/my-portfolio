12ac79066c1680eb4556286f9f9fcfcc1d2b3cb9
azurealstn <azurealstn@naver.com>
2021-05-10 21:36:56 +0900

    포트폴리오 수정

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-## 이력서
-IE에서는 지원되지 않습니다.
+## 포트폴리오
+
+- IE에서는 지원되지 않습니다.
+- [사이트 보기](https://azurealstn.github.io/my-portfolio/)
 
 <br/>
 <br/>
@@ -11,14 +13,19 @@ IE에서는 지원되지 않습니다.
 <br/>
 <br/>
 
-### 제작 기간
+### 참고 사이트 (디자인)
 
-- 2021.04.06 ~ 계속 업데이트 중
+- [fontawesome](https://fontawesome.com/)
+- [로고디자인](https://www.canva.com/ko_kr/)
+- [리사이즈](https://www.iloveimg.com/resize-image)
+
 
 <br/>
 <br/>
 
-### 마크업 구조
+### 마크업 구조 (BEM)
+BEM: Block, Element, Modifier로 분류해 네이밍  
+ex) Block__Element--Modifier
 
 ![캡처](https://user-images.githubusercontent.com/55525868/114045753-3b8be280-98c3-11eb-99f6-2ac4b491eb4c.PNG)
 
@@ -28,6 +35,7 @@ IE에서는 지원되지 않습니다.
 ### 기능
 
 - 반응형 웹 (모바일)
-- 메뉴 버튼을 클릭했을 때 해당 섹션으로 이동 (`scrollIntoView()` 함수 이용)
-- Navigation Bar 상단에 고정
+- Navigation Bar 상단에 고정, 스크롤시 투명 (`scrollY()`, `getBoundingClientRect()` 윈도우창 위치)
+- 메뉴 버튼을 클릭했을 때 해당 섹션으로 이동 (data 속성과 `scrollIntoView()` 이용)
 - `arrow-up` 버튼 클릭시 맨 상단으로 이동
+- Work 섹션에서 필터링 (data 속성으로 key와 value를 주어 비교)
--- main.js
+++ main.js
@@ -21,6 +21,15 @@ menu.addEventListener('click', (event) => {
   event.target.classList.add('active');
 });
 
+function scrollIntoSection(event) {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link === null || link === undefined) return;
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
+
 //CONTACT ME 버튼을 클릭했을 때 Contact 섹션으로 이동
 const homeBtn = document.querySelector('.home__button');
 homeBtn.addEventListener('click', (event) => {
@@ -75,62 +84,4 @@ toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('on');
 });
 
-function scrollIntoSection(event) {
-  const target = event.target;
-  const link = target.dataset.link;
-  if (link === null || link === undefined) return;
 
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-}
-
-/**
- * 1. 모든 섹션 요소들을 가져온다.
- * 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
- * 3. 보여지는 섹션에 해당하는 메뉴 버튼의 active를 활성화한다.
- */
-//1. 모든 섹션 요소들을 가져온다.
-/* const sectionIdList = ['#home', '#about', '#skills', '#work', '#contact'];
-const sections = sectionIdList.map(id => document.querySelector(id));
-const navItems = sectionIdList.map(id => document.querySelector(`[data-link="${id}"]`));
-
-//2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
-let selectedNavItem = navItems[0];
-let selectedNavIndex = 0;
-
-const observerOptions = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.3,
-}
-const callback = (entries, oberserver) => {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
-      const index = sectionIdList.indexOf(`#${entry.target.id}`);
-      //스크롤을 내릴 때 섹션이 위로 올라옴. (y좌표가 마이너스가 됨)
-      if (entry.boundingClientRect.y < 0) {
-        selectedNavIndex = index + 1;
-      } else {
-        selectedNavIndex = index - 1; //스크롤을 올릴 때 섹션이 아래로 내려감. (전 인덱스 선택)
-      }
-    }
-  });
-};
-const oberserver = new IntersectionObserver(callback, observerOptions); //관찰자 생성
-sections.forEach(section => oberserver.observe(section)); //섹션들을 관찰하기
-
-//3. 보여지는 섹션에 해당하는 메뉴 버튼의 active를 활성화한다.
-function selectNavItem(selector) {
-  selectedNavItem.classList.remove('active'); //첫번째 아이템 active 클래스를 지운다.
-  selectedNavItem = selector;
-  selectedNavItem.classList.add('active');
-}
-
-window.addEventListener('wheel', () => {
-  if (window.screenY === 0) {
-    selectedNavIndex = 0;
-  } else if (window.screenY + window.innerHeight === document.body.clientHeight) {
-    selectedNavIndex = navItems.length - 1;
-  }
-  selectNavItem(navItems[selectedNavIndex]);
-}); */
